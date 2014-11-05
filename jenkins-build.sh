4cc517269ad51027e1f2e67fee9e81f8aed1d3a4
Jim Fathman <jfathman@esi-estech.com>
2014-11-05 16:31:00 +0000

    Invoke fig instead of docker.

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -15,10 +15,13 @@ main()
 
   APP_VERSION=$(cat package.json | jq -r '.version')
 
+  FIG_NAME=$(echo $APP_NAME | sed 's/-//g')_app
+
   set +x
   annotate "Build Docker image."
 
-  docker build -t ${APP_NAME}:${APP_VERSION} .
+  fig build
+  docker tag ${FIG_NAME}:latest ${FIG_NAME}:${APP_VERSION}
 
   set +x
   annotate "Remove untagged images after Docker reuses repo:tag for new build."
@@ -32,29 +35,31 @@ main()
   set +x
   annotate "Run mock tests including load test in Docker container."
 
-  docker run --rm ${APP_NAME}:${APP_VERSION} grunt --no-color test
+  fig run --rm app grunt --no-color test
+  fig stop
+  fig rm --force
 
   set +x
   annotate "Retrieve build artifacts from Docker container."
 
   mkdir -p artifacts
 
-  docker run --rm -v ${PWD}/artifacts:/mnt ${APP_NAME}:${APP_VERSION} /bin/bash -c 'cp artifacts/* /mnt/.'
+  fig run --rm --no-deps app bash -c 'cp artifacts/* /mnt/.'
 
   set +x
   annotate "Tag Docker image for Artifactory."
 
-  docker tag ${APP_NAME}:${APP_VERSION} ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
+  docker tag ${FIG_NAME}:${APP_VERSION} ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${FIG_NAME}:${APP_VERSION}
 
   set +x
   annotate "Push Docker image to Artifactory."
 
-  docker push ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
+  docker push ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${FIG_NAME}:${APP_VERSION}
 
   set +x
   annotate "Remove tag added for Artifactory."
 
-  docker rmi ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
+  docker rmi ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${FIG_NAME}:${APP_VERSION}
 
   set +x
   annotate "Build complete."
